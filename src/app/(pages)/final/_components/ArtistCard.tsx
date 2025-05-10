664e91dd9792ce9310edee08901a869ac0ef46bb
minseo <yeominseo59618@gmail.com>
2025-05-10 23:58:00 +0900

    이미지 클릭 시 상세페이지 전환 구현

--- src/app/(pages)/final/_components/ArtistCard.tsx
+++ src/app/(pages)/final/_components/ArtistCard.tsx
@@ -1,6 +1,9 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 interface ArtistCardProps {
+  id: string;
   name: string;
   score: number;
   image: string;
@@ -8,25 +11,33 @@ interface ArtistCardProps {
 }
 
 export default function ArtistCard({
+  id,
   name,
   score,
   image,
   onVote,
 }: ArtistCardProps) {
   const [voted, setVoted] = useState(false);
-
+  const router = useRouter();
   const handleVote = () => {
     onVote();
     setVoted(true);
   };
+
+  //밴드 페이지로 전환
+  const handleImageClick = () => {
+    router.push('/band/${id}');
+  };
   return (
-    <div>
+    <div className='text-center'>
       <img
-        src='/image/dog.png'
-        className='w-full h-40 object-cover mb-2 rounded'
+        src={image}
+        alt={`${name} 이미지`}
+        onClick={handleImageClick}
+        className='w-full h-40 object-cover mb-2 rounded cursor-pointer transition hover:opacity-80'
       />
-      <p className='font-bold text-center'>{name}</p>
-      <p className='text-sm text-center'>점수:{score}</p>
+      <p className='font-bold'>{name}</p>
+      <p className='text-sm'>점수:{score}</p>
       <button
         onClick={handleVote}
         className={`mt-1 text-white text-sm px-3 py-1 rounded-md ${
