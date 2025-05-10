07e012b972747236d6adef9bf747f411dd5bef4f
minseo <yeominseo59618@gmail.com>
2025-05-10 21:11:18 +0900

    artist card, artistgrid 코드 추가

--- src/app/(pages)/final/final_components/ArtistCard.tsx
+++ src/app/(pages)/final/final_components/ArtistCard.tsx
@@ -1,13 +1,28 @@
-export default function ArtistCard() {
+interface ArtistCardProps {
+  name: string;
+  score: number;
+  image: string;
+  onvote: () => void;
+}
+
+export default function ArtistCard({
+  name,
+  score,
+  image,
+  onvote,
+}: ArtistCardProps) {
   return (
     <div>
       <img
         src='/image/dog.png'
         className='w-full h-40 object-cover mb-2 rounded'
       />
-      <p className='font-bold text-center'>이름</p>
-      <p className='text-sm text-center'>점수:</p>
-      <button className='mt-1 bg-gray-500 text-white text-sm px-3 py-1 rounded-md'>
+      <p className='font-bold text-center'>{name}</p>
+      <p className='text-sm text-center'>점수:{score}</p>
+      <button
+        onClick={onvote}
+        className='mt-1 bg-gray-500 text-white text-sm px-3 py-1 rounded-md'
+      >
         투표하기
       </button>
     </div>
--- src/app/(pages)/final/page.tsx
+++ src/app/(pages)/final/page.tsx
@@ -1,11 +1,24 @@
+import { mock } from 'node:test';
 import ArtistCard from './final_components/ArtistCard';
+import ArtistGrid from './final_components/ArtistGrid';
 import PageHeader from './final_components/PageHeader';
 
+const mockArtists = Array.from({ length: 12 }).map((_, i) => ({
+  id: `${i + 1}`,
+  name: `이름 ${i + 1}`,
+  image: '', // 이미지 경로 또는 비워둬도 됨
+  score: 333,
+}));
+
 export default function Final() {
+  const handleVote = (id: string) => {
+    alert('${id}번 아티스트에게 투표했습니다!');
+  };
+
   return (
     <div className='p-8'>
       <PageHeader title='본선진출' />
-      <ArtistCard />
+      <ArtistGrid artists={mockArtists} onVote={handleVote} />
     </div>
   );
 }
