af58e15b6f5668d9709b7b948e3f68457669b6c1
LeeYonDu <gj5051129@gmail.com>
2025-05-11 00:55:01 +0900

    feat:videoPlayer

--- src/app/(pages)/band/[band_id]/_components/band_detail.tsx
+++ src/app/(pages)/band/[band_id]/_components/band_detail.tsx
@@ -4,22 +4,25 @@ export default function Detail() {
   return (
     <div className='w-full h-full flex justify-center items-center'>
       <div className='flex justify-center items-start w-full bg-blue-300/30'>
-        <div>
-          <div className='flex-1 flex justify-between p-2 items-center flex-row '>
-            <div className='flex-1 flex justify-left gap-4 items-center flex-row p-2'>
-              <div className='w-24'>
+        <div className='flex-1'>
+          {/* 상단 섹션 */}
+          <div className='flex justify-between items-center p-3'>
+            {/* 앨범커버 + 곡명 + 팀명 */}
+            <div className='flex items-center gap-4'>
+              <div className='w-16'>
                 <img src='/img/cover.jpg' alt='앨범커버' />
               </div>
-
-              <div>
-                <div className='text-l'>Hello,Wonderland</div> {/*곡 명*/}
-                <div className='text-m'>Lacuna</div> {/*팀 명*/}
+              <div className='flex flex-col justify-center'>
+                <div className='text-l'>Hello, Wonderland</div>
+                <div className='text-m'>Lacuna</div>
               </div>
             </div>
 
+            {/* 재생 버튼 */}
             <button className='w-16 h-16 rounded-full bg-black/30'>재생</button>
           </div>
 
+          {/* 하단 재생바 */}
           <div>재생바</div>
         </div>
       </div>
---
+++ src/app/(pages)/band/[band_id]/_components/band_video.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Detail from './band_detail';
+import BackButton from './band_backButton';
+
+export default function Video() {
+  return (
+    <div className='w-full h-full flex justify-center items-center'>
+      <div className='flex justify-center items-start w-full aspect-[16/9] bg-red-100 relative'>
+        <div className='absolute top-0 z-1'>
+          <BackButton />
+        </div>
+        <video
+          className='w-full aspect-[16/9] shadow-md'
+          controls
+          preload='none'
+        >
+          <source src='/path/to/video.mp4' type='video/mp4' />
+          <track
+            src='/path/to/captions.vtt'
+            kind='subtitles'
+            srcLang='en'
+            label='English'
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+}
