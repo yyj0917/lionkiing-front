1f39c86edb2d7720c33b5cb1951e4454af0a466f
eric-jy-park <2019147551@yonsei.ac.kr>
2025-05-11 06:50:59 +0900

    revert: revert final page

--- src/app/(pages)/final/page.tsx
+++ src/app/(pages)/final/page.tsx
@@ -1,34 +1,38 @@
-/* eslint-disable */
-
 'use client';
 
 import { useEffect, useState } from 'react';
 import ArtistGrid from './_components/ArtistGrid';
 import PageHeader from './_components/PageHeader';
 import VoteModal from './_components/VoteModal';
+import { getBandFinalInfo } from '@/app/_common/apis/band-final-info';
+import { getAvailableVotes } from '@/app/_common/apis/available-votes';
+import { BandFinalInfo } from '@/app/_common/interfaces/band-info.interface';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import TVDropZone from '../(home)/_components/tv-dropzone';
-import { getBandFinalInfo } from '@/app/_common/apis/band-final-info';
-import { getAvailableVotes } from '@/app/_common/apis/available-votes';
+import RankBoard from '../(home)/_components/rank-board';
+import VoteSection from './_components/Votesection';
+import Mileage from './_components/Mileage';
 
-const mockArtists = Array.from({ length: 12 }).map((_, i) => ({
-  id: `${i + 1}`,
-  name: `이름 ${i + 1}`,
-  image: '/image/dog.png',
-  score: 0,
-}));
+interface Artist {
+  id: string;
+  name: string;
+  image: string;
+  score: number;
+}
 
 export default function Final() {
   const [refetch, setRefetch] = useState(false);
   const [selectedId, setSelectedId] = useState<string | null>(null);
-  const [artists, setArtists] = useState(mockArtists);
+  const [artists, setArtists] = useState<Artist[]>([]);
+  const [bandFinalInfo, setBandFinalInfo] = useState<BandFinalInfo[]>([]);
   const [votedIds, setVotedIds] = useState<string[]>([]);
   const [mileage, setMileage] = useState<number>(0);
 
   useEffect(() => {
     const fetchData = async () => {
       const bandData = await getBandFinalInfo();
+      setBandFinalInfo(bandData);
 
       const mapped = bandData.map(band => ({
         id: band.id,
@@ -54,13 +58,23 @@ export default function Final() {
   };
 
   return (
-    <div className='p-8'>
+    <div
+      className='p-8'
+      style={{
+        backgroundImage: 'url(/images/final-background.png)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+      }}
+    >
       <DndProvider backend={HTML5Backend}>
-        <PageHeader title='본선진출' />
+        <PageHeader title='2025 청춘무대 본선' />
         <TVDropZone />
+        <RankBoard bandFinalInfo={bandFinalInfo} />
+        <VoteSection vote='투표하기' />
+        <Mileage mileage={mileage} />
         <ArtistGrid artists={artists} onVote={handleVote} votedIds={votedIds} />
-
-        {selectedId && (
+        {selectedId && mileage !== null && (
           <VoteModal
             artistId={selectedId}
             onClose={closeModal}
